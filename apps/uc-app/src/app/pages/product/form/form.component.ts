777f7b2fbb677084a88ebf7fab1b2064422190a1
Calvin <cl.kleijn@student.avans.nl>
2022-11-19 18:13:46 +0100

    Finish crud for product with imdb and frontend validation + styling

--- apps/uc-app/src/app/pages/product/form/form.component.ts
+++ apps/uc-app/src/app/pages/product/form/form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 
@@ -11,28 +10,39 @@ import { ProductService } from '../product.service';
 })
 export class FormComponent implements OnInit {
   productId: string | null = null;
-  product = new Product();
-  product$: Observable<Product> | undefined;
+  productName: string | null = null;
+  productExists: boolean = false;
+  product: Product | undefined;
 
   constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.route.paramMap
-      .pipe(
-        switchMap((params: ParamMap) => {
-          this.productId = params.get('productId');
-          return this.productService.getProductById(Number(this.productId));
-        })
-      )
-      .subscribe((product) => {
+    this.route.paramMap.subscribe(params => {
+      this.productId = params.get('productId');
+
+      if (this.productId) {
+        this.productExists = true;
         this.product = {
-          ...product
-        };
-      });
+          ...this.productService.getProductById(Number(this.productId))
+        }
+        this.productName = '(' + this.product.brand + ') ' + this.product.name;
+      } 
+      else {
+        this.productExists = false;
+        this.product = new Product();
+        this.product._id = this.productService.getNewIndex();
+      }
+    })
   }
 
   onSubmit(): void {
-    this.productService.updateProduct(Number(this.productId), this.product);
-    this.router.navigate(['products']);
+    if (this.productExists) {
+      this.productService.updateProduct(Number(this.productId), this.product!);
+    } 
+    else {
+      this.productService.createProduct(this.product!);
+    }
+
+    this.router.navigate(['products'])
   }
 }
