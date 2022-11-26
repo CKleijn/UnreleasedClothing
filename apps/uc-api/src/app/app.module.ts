68848bc208a35183e68d25f3dda91b9aeef96476
Calvin <cl.kleijn@student.avans.nl>
2022-11-26 13:43:24 +0100

    Add read for strong entity rating

--- apps/uc-api/src/app/app.module.ts
+++ apps/uc-api/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { CommentModule } from './comment/comment.module';
 import { ProductModule } from './product/product.module';
+import { RatingModule } from './rating/rating.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/uc-db'), AuthModule, UserModule, ProductModule, CategoryModule, CommentModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/uc-db'), AuthModule, UserModule, ProductModule, CategoryModule, CommentModule, RatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
